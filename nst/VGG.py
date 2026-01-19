8bbde1b8bd57ae2c53e471db4608876a949809ef
prabhsuratsingh <prabhsurat17@gmail.com>
2026-01-20 00:26:59 +0530

    add Average pool layer to VGG

--- nst/VGG.py
+++ nst/VGG.py
@@ -4,28 +4,32 @@ import torchvision.models as models
 class VGGFeatures(nn.Module):
     def __init__(self):
         super().__init__()
-        vgg = models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1).features
+        vgg = models.vgg19(
+            weights=models.VGG19_Weights.IMAGENET1K_V1
+        ).features
+
+        for i, layer in enumerate(vgg):
+            if isinstance(layer, nn.MaxPool2d):
+                vgg[i] = nn.AvgPool2d(kernel_size=2, stride=2)
 
         self.layers = {
-            "0": "conv1_1",
-            "5": "conv2_1",
+            "0":  "conv1_1",
+            "5":  "conv2_1",
             "10": "conv3_1",
             "19": "conv4_1",
             "21": "conv4_2",
             "28": "conv5_1",
         }
 
-        self.model = vgg[:29]
+        self.model = vgg[:29].eval()
 
         for p in self.model.parameters():
             p.requires_grad = False
-    
+
     def forward(self, x):
         features = {}
-
         for name, layer in self.model._modules.items():
             x = layer(x)
             if name in self.layers:
                 features[self.layers[name]] = x
-        
         return features
